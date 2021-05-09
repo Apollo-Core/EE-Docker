6f69e843c403bb827f22be717ff48fe1b614c693
Fedor <fedor@dps.uibk.ac.at>
2021-05-09 09:15:20 +0200

    removed the server from the module

--- src/main/java/at/uibk/dps/ee/docker/modules/LocalContainersModule.java
+++ src/main/java/at/uibk/dps/ee/docker/modules/LocalContainersModule.java
@@ -1,11 +1,7 @@
 package at.uibk.dps.ee.docker.modules;
 
-import org.opt4j.core.config.annotations.Info;
-import org.opt4j.core.config.annotations.Order;
-import org.opt4j.core.start.Constant;
 import at.uibk.dps.ee.docker.manager.ContainerManager;
 import at.uibk.dps.ee.docker.manager.ContainerManagerExec;
-import at.uibk.dps.ee.docker.server.ServerStarter;
 import at.uibk.dps.ee.guice.modules.FunctionModule;
 
 /**
@@ -15,22 +11,8 @@ import at.uibk.dps.ee.guice.modules.FunctionModule;
  */
 public class LocalContainersModule extends FunctionModule {
 
-  @Order(1)
-  @Info("The port used by the container manager.")
-  @Constant(value = "portNumber", namespace = ServerStarter.class)
-  public int portNumber = 5055;
-
   @Override
   protected void config() {
     bind(ContainerManager.class).to(ContainerManagerExec.class);
-    bind(ServerStarter.class).asEagerSingleton();
-  }
-
-  public int getPortNumber() {
-    return portNumber;
-  }
-
-  public void setPortNumber(int portNumber) {
-    this.portNumber = portNumber;
   }
 }
